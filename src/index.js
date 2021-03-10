41abfa76c4d3f6e9da6d2b8d633ced3070e9c315
FeralDaro <meral.nekrag@gmail.com>
2021-03-10 04:02:05 +0300

    feat: create a bracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let closeBrackets = [],
+        openBrackets = [],
+        stack = [];
+    for (bracket of bracketsConfig) {
+        openBrackets.push(bracket[0])
+    }
+    for (bracket of bracketsConfig) {
+        closeBrackets.push(bracket[1])
+    }
+    let closeIndex;
+    let openIndex;
+    for (let i = 0, len = str.length; i < len; i++) {
+        openIndex = openBrackets.indexOf(str[i])
+        closeIndex = closeBrackets.indexOf(str[i])
+        if(openIndex !== -1 && closeIndex !== -1){
+            let stackIndex = stack.indexOf(openIndex);
+            if(stackIndex === -1){
+                stack.push(openIndex);
+                continue;
+            }
+            if(stackIndex !== -1){
+                openIndex = stack.pop();
+            }
+        }
+
+        if (openIndex !== -1 && closeIndex === -1) {
+            stack.push(openIndex);
+            continue
+        }
+
+        if (closeIndex !== -1 && openIndex === -1) {
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false
+            }
+        }
+    }
+    return stack.length === 0;
+    
 }
